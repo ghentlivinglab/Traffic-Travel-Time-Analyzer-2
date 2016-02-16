ae2aa5ee4912d92f6477cd68e8f87adb53d580b9
Tobias Van der Pulst <tobiasvdp@outlook.com>
2016-02-16 19:26:02 +0100

    Test for transfer of beans. Failed to import last projects on my end.

---
+++ Realisatie/GoogleMapsSourceAdaptor/src/java/iii/vop2016/verkeer2/ejb/provider/GoogleMapsSourceAdaptor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iii.vop2016.verkeer2.ejb.provider;
+
+import javax.ejb.Singleton;
+
+/**
+ *
+ * @author tobia
+ */
+@Singleton
+public class GoogleMapsSourceAdaptor implements GoogleMapsSourceAdaptorRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
