2de1bf69b7cd148f03c919f8a34d84854a30b746
Tobias VdP <tobiasvdp@outlook.com>
2016-04-30 14:42:24 +0200

    push to pc testfiles bean mem usage

--- Realisatie/TrafficDataDownstreamAnalyser/src/java/iii/vop2016/verkeer2/ejb/downstream/TrafficDataDownstreamAnalyser.java
+++ Realisatie/TrafficDataDownstreamAnalyser/src/java/iii/vop2016/verkeer2/ejb/downstream/TrafficDataDownstreamAnalyser.java
@@ -75,18 +75,25 @@ public class TrafficDataDownstreamAnalyser implements TrafficDataDownstreamAnaly
     @Override
     public void endSession(List<IRouteData> data, List<IRoute> routes) {
         LoggerRemote logger = beans.getLogger();
-         logger.log(Level.FINER, "Ending analysis session.");
+        logger.log(Level.FINER, "Ending analysis session. routes:" +data.size());
         IDataProvider dataProvider = beans.getDataProvider();
         IThresholdManager threshold = beans.getThresholdManager();
 
+        logger.log(Level.FINER, "Mapping data to routes");
         Map<Long, List<IRouteData>> mapping = MapDataToRoutes(data);
+        logger.log(Level.FINER, "Endee mapping data to routes : " + mapping.size());
+        
 
         for (IRoute route : routes) {
+            logger.log(Level.FINER, route.getName() + " eval");
             List<IRouteData> routeData = mapping.get(route.getId());
-            if (routeData != null) {
-                int opt = dataProvider.getOptimalDuration(route, null);
+            if (routeData != null && routeData.size() != 0) {
+                logger.log(Level.FINER, route.getName() + " eval opt and mean");
+                int opt = dataProvider.getOptimalDuration(route, new ArrayList<String>());
                 int mean = dataProvider.getMeanDurationFromRouteData(routeData);
+                logger.log(Level.FINER, route.getName() + "end  eval opt and mean");
                 if (opt != -1 && mean != -1) {
+                    logger.log(Level.FINER, route.getName() + " eval threshold");
                     int delay = mean - opt;
                     threshold.EvalThresholdLevel(route, delay);
                 }
@@ -99,8 +106,11 @@ public class TrafficDataDownstreamAnalyser implements TrafficDataDownstreamAnaly
 
     private Map<Long, List<IRouteData>> MapDataToRoutes(List<IRouteData> routeData) {
         Map<Long, List<IRouteData>> ret = new HashMap<>();
+        LoggerRemote logger = beans.getLogger();
+        logger.log(Level.FINER, "MapDataToRoutes.");
 
         for (IRouteData r : routeData) {
+            logger.log(Level.FINER, r.getId() + " - " + r.getRouteId());
             List<IRouteData> data = ret.get(r.getRouteId());
             if (data == null) {
                 data = new ArrayList<>();
